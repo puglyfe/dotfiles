a7706923fb69508fdba3ae461467d4722d50da99
Charley <ctpugmire@gmail.com>
2022-01-31 10:52:57 -0600

    update git settings

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,3 @@
 sudo ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 sudo ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
-sudo ln -s ~/.dotfiles/git/pantheon/.gitconfig ~/.gitconfig-pantheon
+sudo ln -s ~/.dotfiles/git/fast/.gitconfig ~/.gitconfig-fast
